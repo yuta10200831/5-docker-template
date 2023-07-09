75a4b9f1d054a0397a41d40aa5c0e626bf700cbe
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-09 12:05:14 +0900

    5. foreach文の課題提出

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,7 @@
-<?php
-echo 'Welcome TECH QUEST!';
+<?php 
+$countries = ["ブラジル", "日本", "スペイン"];
+
+foreach ($countries as $number => $countryName) {
+  $position = $number + 1;
+  echo $position . "番目: " . $countryName . "が入っています。" . "\n";
+}
